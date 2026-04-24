be9084e20bf514e2b8c8a702613aef5a55b92ad4
dineshmedagodasec <dineshmedagoda.sec@gmail.com>
2026-04-24 08:18:17 +0530

    Update youtube_video_maker.py

--- youtube_video_maker.py
+++ youtube_video_maker.py
@@ -84,7 +84,7 @@ def create_youtube_short(quote, author, image_path):
             stroke_color="black",
             stroke_width=1
         )
-        author_clip = author_clip.with_position(("center", 1000))
+        author_clip = author_clip.with_position(("center", 1200))
         author_clip = author_clip.with_start(3)
         author_clip = author_clip.with_duration(duration - 3)
 
